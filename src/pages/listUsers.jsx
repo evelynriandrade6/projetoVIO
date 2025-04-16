22cb305cf98e0077a9a286c3f6581f83bdb3103b
evelynriandrade6 <eveyandrade0.3@gmail.com>
2025-04-16 16:15:27 -0300

    16/04 dps eu termino isso

--- src/pages/listUsers.jsx
+++ src/pages/listUsers.jsx
@@ -1,86 +1,119 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from "react";
 // Imports para criação de tabela
-import Table from '@mui/material/Table';
-import TableContainer from '@mui/material/TableContainer';
+import Table from "@mui/material/Table";
+import TableContainer from "@mui/material/TableContainer";
 // TableHead é onde colocamos os titulos
-import TableHead from '@mui/material/TableHead';
+import TableHead from "@mui/material/TableHead";
 // TableBody é onde colocamos o conteúdo
-import TableBody from '@mui/material/TableBody';
-import TableRow from '@mui/material/TableRow';
-import TableCell from '@mui/material/TableCell';
-import Paper from '@mui/material/Paper';
-import api from '../axios/axios'
-import { Button } from '@mui/material';
-import { Link, useNavigate } from 'react-router-dom';
+import TableBody from "@mui/material/TableBody";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import Paper from "@mui/material/Paper";
+import api from "../axios/axios";
+
+import { Button, IconButton, Alert, Snackbar } from "@mui/material";
+import DeleteOutlineIcon from "@mui/icons-material/DeleteOutline";
+import DeleteIcon from "@mui/icons-material/Delete";
+
+import { Link, useNavigate } from "react-router-dom";
 
 function listUsers() {
-  const [users,setUsers] = useState([]);
-  const navigate = useNavigate();
+  const [users, setUsers] = useState([]);
+  const [alert, setAlert] = useState({
+    // Visibilidade (false = oculto; true: visível)
+    open: false,
 
-  async function getUsers(){
+    // Nível do alerta (success, error, warning, etc)
+    severity: "",
+
+    // Mensagem que será exibida
+    message: ""
+  });
+
+  // Função para exibir o alerta
+  const showAlert = (severity, message) => {
+    setAlert({open: true, severity, message})
+  };
+
+  // Fechar o alerta
+  const handleCloseAlert = () => {
+    setAlert({ ...alert, open: false})
+  };
+
+  const navigate = useNavigate();
+  async function getUsers() {
     // Chamada da Api)
     await api.getUsers().then(
-      (response)=>{
-        console.log(response.data.users)
-        setUsers(response.data.users)
-      },(error)=>{
-        console.log("Erro ",error)
+      (response) => {
+        console.log(response.data.users);
+        setUsers(response.data.users);
+      },
+      (error) => {
+        console.log("Erro ", error);
       }
-    )
+    );
   }
 
-  const listUsers = users.map((user)=>{
-    return(
+  async function deleteUser(id){
+    try{
+      await api.deleteUser(id);
+      await getUsers();
+      showAlert("success", "Usuário excluído com sucesso!");
+    }
+    catch(error){
+      console.log("Erro ao deletar usuário...", error);
+      showAlert("error", error.response.data.error);
+    }
+  }
+
+  const listUsers = users.map((user) => {
+    return (
       <TableRow key={user.id_usuario}>
         <TableCell align="center">{user.name}</TableCell>
         <TableCell align="center">{user.email}</TableCell>
         <TableCell align="center">{user.cpf}</TableCell>
-      </TableRow>
-    )
-  })
 
-  function logout(){
+        <TableCell align="center">
+          <IconButton onClick={() => deleteUser(user.id)}>
+            <DeleteIcon color="error" />
+          </IconButton>
+        </TableCell>
+      </TableRow>
+    );
+  });
+
+  function logout() {
     localStorage.removeItem("authenticated");
-    navigate("/")
+    navigate("/");
   }
 
-  useEffect(()=>{
-    if(!localStorage.getItem("authenticated")){
+  useEffect(() => {
+    if (!localStorage.getItem("authenticated")) {
       navigate("/");
     }
     getUsers();
-  },[]);
-
+  }, []);
 
   return (
     <div>
-        <h5>Lista de usuários</h5>
-        <TableContainer component={Paper} style={{margin:"2px"}}>
-          <Table size="small">
-            <TableHead style={{backgroundColor: "brown", borderStyle:"solid"}}>
-              <TableRow>
-                <TableCell align="center">
-                  Nome
-                </TableCell>
-                <TableCell align="center">
-                  Email
-                </TableCell>
-                <TableCell align="center">
-                  CPF
-                </TableCell>
-              </TableRow>
-            </TableHead>
-            <TableBody>{listUsers}</TableBody>
-          </Table>
-        </TableContainer>
-      <Button 
-      fullWidth
-      variant='contained'
-      onClick={logout}
-      >
+      <h5>Lista de usuários</h5>
+      <TableContainer component={Paper} style={{ margin: "2px" }}>
+        <Table size="small">
+          <TableHead style={{ backgroundColor: "brown", borderStyle: "solid" }}>
+            <TableRow>
+              <TableCell align="center">Nome</TableCell>
+              <TableCell align="center">Email</TableCell>
+              <TableCell align="center">CPF</TableCell>
+              <TableCell align="center">Ações</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>{listUsers}</TableBody>
+        </Table>
+      </TableContainer>
+      <Button fullWidth variant="contained" onClick={logout}>
         SAIR
       </Button>
     </div>
-  )
+  );
 }
-export default listUsers
+export default listUsers;
