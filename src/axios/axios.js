a8e3d8cd79794ceead64bba5621fafbd8797e258
evelynriandrade6 <eveyandrade0.3@gmail.com>
2025-08-18 14:09:07 -0300

    18/08 token expirado

--- src/axios/axios.js
+++ src/axios/axios.js
@@ -18,6 +18,30 @@ api.interceptors.request.use(
   },
   (error) => Promise.reject(error)
 );
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 401 && error.response.data.auth === false) {
+        localStorage.setItem("refresh_token", true);
+        localStorage.removeItem("token");
+        localStorage.removeItem("authenticated");
+        window.location.href = "/";
+      } else if (error.response) {
+        (
+          error.response.status === 403 &&
+          error.response.data.auth === false
+        )
+          localStorage.setItem("refresh_token", true);
+          localStorage.removeItem("token");
+          localStorage.removeItem("authenticated");
+          window.location.href = "/";
+        }
+      }
+      return Promise.reject(error);
+    }
+);
 const sheets = {
   getUsers: () => api.get("user"),
   postLogin: (user) => api.post("user/login", user),
@@ -29,15 +53,15 @@ const sheets = {
   createEvento: (form, imagem) => {
     const data = new FormData();
     for (let key in form) data.append(key, form[key]);
-    if(imagem) data.append("imagem", imagem);
+    if (imagem) data.append("imagem", imagem);
 
     return api.post("/evento", data, {
-      headers:{
-        "Content-Type":"multpart/form-data",
-        Accept:"application/json",
-      }
-    })
-  }
+      headers: {
+        "Content-Type": "multpart/form-data",
+        Accept: "application/json",
+      },
+    });
+  },
 };
 
 export default sheets;
